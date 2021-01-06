0e1ef9eaf710dbf29890687243a9cf07ca93f3bf
Vinicius Bignardi <vinicius.bignardi@outlook.com>
2021-01-05 22:50:00 -0300

    POST /orders

--- backend/src/main/java/com/vbt/godelivery/services/OrderService.java
+++ backend/src/main/java/com/vbt/godelivery/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.vbt.godelivery.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vbt.godelivery.dto.OrderDTO;
+import com.vbt.godelivery.dto.ProductDTO;
 import com.vbt.godelivery.entities.Order;
+import com.vbt.godelivery.entities.OrderStatus;
+import com.vbt.godelivery.entities.Product;
 import com.vbt.godelivery.repositories.OrderRepository;
+import com.vbt.godelivery.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -18,6 +23,9 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	// Garante a transação com o Banco de Dados
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
@@ -25,4 +33,15 @@ public class OrderService {
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()); // Passando os dados de Product para ProductDTO usando Lambda
 	}
 	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), 
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
 }
